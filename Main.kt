d627180ea253879d03af3437da3cf77eca9ed4fa
AlbertKloster <albert.kloster@gmail.com>
2023-06-28 12:09:36 -0400

    Stage1

--- Main.kt
+++ Main.kt
@@ -1,5 +1,16 @@
 package indigo
 
 fun main() {
-    println("Hello World!")
+    val ranks = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+    val suits = listOf("♦", "♥", "♠", "♣")
+    val deck = mutableListOf<String>()
+    for (rank in ranks) {
+        for (suit in suits) {
+            deck.add(rank + suit)
+        }
+    }
+
+    println(ranks.joinToString(" "))
+    println(suits.joinToString(" "))
+    println(deck.joinToString(" "))
 }
